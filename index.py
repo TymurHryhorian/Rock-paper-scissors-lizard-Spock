f9282c2481430c4c0263055b20f18145c9287d0b
mechmatKNU <106305386+mechmatKNU@users.noreply.github.com>
2022-05-26 11:28:47 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import random
 
+
 # 0 - камінь
 # 1 - Спок
 # 2 - папір
@@ -16,47 +17,43 @@ def name_to_number(name):
     elif name == "хамелеон":
         return 3
     elif name == "ножиці":
-        return 4        
+        return 4
 
-def number_to_name(number):
-    if number == 0:
+
+def number_to_name(code):
+    if code == 0:
         return "камінь"
-    elif number == 1:
+    elif code == 1:
         return "Спок"
-    elif number == 2:
+    elif code == 2:
         return "папір"
-    elif number == 3:
+    elif code == 3:
         return "хамелеон"
-    elif number == 4:
+    elif code == 4:
         return "ножиці"
-    else:
-        print "number_to_name() find unkown number:",number
-        
-def rpsls(player_choice): 
-    """ 
-    player_choice: the name of the player choice
-    """
+
+def rpsls(player_choice):
 
     print("\n")
-    
-    print "Гравець обрав "+player_choice
-    
-    player_number = name_to_number(player_choice)
-    
+
+    print("Гравець обрав " + player_choice)
+
+    player_code = name_to_number(player_choice)
+
     # хід ПК
-    comp_number = random.randrange(0,5)
-    comp_choice = number_to_name(comp_number)
-    print ("Computer chooses "+comp_choice)
-    
-    diff = (comp_number - player_number) % 5
-    
+    bot_code = random.randrange(0, 5)
+    comp_choice = number_to_name(bot_code)
+    print("Комп'ютер обрав " + comp_choice)
+
+    diff = (bot_code - player_code) % 5
+
     if (diff == 1) or (diff == 2):
-        print ("Комп'ютер виграв!")
+        print("Комп'ютер виграв!")
     elif (diff == 3) or (diff == 4):
         print("Гравець виграв!")
     else:
         print("Нічия")
-                         
+
 rpsls("камінь")
 rpsls("Спок")
 rpsls("папір")
